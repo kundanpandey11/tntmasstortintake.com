432d9e9918f17be40cfc914304659b9a115e6b3e
Ankit varma <ankitvarma7000@gmail.com>
2024-12-04 21:52:57 +0530

    updated images, chnaged domain, added #

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+
+class DynamicTemplateView(TemplateView):
+    def get_template_names(self):
+        return [self.kwargs['template_name']]
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("leads.urls"), name="leads")
+    path("", include("leads.urls"), name="leads"),
+    path('template/<str:template_name>', 
+         DynamicTemplateView.as_view(),
+         name='template'),
 ]
